83e98093c76d9880cbcad8500c9b8ce7269e9aab
Jashua <77981430+81Jashua@users.noreply.github.com>
2023-09-13 18:56:36 -0600

    W02 Learning activity progress

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,17 +3,20 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
-
+let fullName = 'Joshua Snyder'
+let currentYear = new Date().getFullYear();
+let profilePicture = 'images/profileimage.jpg'
 
 /* Step 3 - Element Variables */
-
-
-
+const nameElement = document.getElementById('name')
+const foodElement = document.getElementById('food')
+const yearElement = document.querySelector('#year')
+const imageElement = document.querySelector('img')
+//const profilePicture = document.getElementById('')
 
 
 /* Step 4 - Adding Content */
+document.querySelector('imageElement').setAttribute('src', profilePicture);
 
 
 
