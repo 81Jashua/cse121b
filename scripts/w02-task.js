ee5a311b8cacbfbed27e24ce387f06ebc5d1251a
Jashua <77981430+81Jashua@users.noreply.github.com>
2023-09-13 21:42:10 -0600

    W02 LA complete

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -11,19 +11,25 @@ let profilePicture = 'images/profileimage.jpg'
 const nameElement = document.getElementById('name')
 const foodElement = document.getElementById('food')
 const yearElement = document.querySelector('#year')
-const imageElement = document.querySelector('img')
-//const profilePicture = document.getElementById('')
-
+const imageElement = document.querySelector("img"); 
 
 /* Step 4 - Adding Content */
-document.querySelector('imageElement').setAttribute('src', profilePicture);
-
-
-
-
-
+nameElement.innerHTML = `<strong>${fullName}<strong>`
+yearElement.textContent = currentYear
+imageElement.setAttribute('src', profilePicture)
+imageElement.setAttribute('alt', `Profile image of ${fullName}`)
 
 /* Step 5 - Array */
+let favoriteFoods = ['Smoothie','Pizza','Hamburger']
+foodElement.innerHTML = favoriteFoods
+let anotherFood = 'Spagetti'
+favoriteFoods.push(anotherFood)
+foodElement.innerHTML += `<br>${favoriteFoods}`
+favoriteFoods.shift()
+foodElement.innerHTML += `<br>${favoriteFoods}`
+favoriteFoods.pop()
+foodElement.innerHTML += `<br>${favoriteFoods}`
+
 
 
 
