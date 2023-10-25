b0b9266622a30f12eaee27732bf105b05e59a93d
Jashua <77981430+81Jashua@users.noreply.github.com>
2023-10-24 21:50:32 -0600

    project

--- scripts/project.js
+++ scripts/project.js
@@ -1,30 +1,24 @@
 const dogElement = document.getElementById('dog');
+const favElement = document.getElementById('favoriteDogs');
+
 let dogList = [];
+let favDogs = [];
 let dog = '';
 document.getElementById("searchBtn").addEventListener('click', getDogType);
-
+document.getElementById("favoriteBtn").addEventListener('click', addFavorite);
 let options = {
     method: 'GET',
     headers: { 'x-api-key': 'q+zNrzUAjK/k0BOwxHFWww==rgrhTdojd1pvQPaM' }
   }
   
 let url = 'https://api.api-ninjas.com/v1/dogs?name=golden retriever'
-//let url = `https://api.api-ninjas.com/v1/dogs?name=${dog}`;
   
 function getDogType(){
     dog = document.getElementById("dogSearch").value;
     url = `https://api.api-ninjas.com/v1/dogs?name=${dog}`;
+    reset();
     getDog();
-    //`Profile image of ${fullName}`
 }
-//   fetch(url,options)
-//         .then(res => res.json()) // parse response as JSON
-//         .then(data => {
-//           console.log(data)
-//         })
-//         .catch(err => {
-//             console.log(`error ${err}`)
-//         });
         
 /* async getTemples Function using fetch()*/
 const getDog = async () => {
@@ -38,7 +32,6 @@ const getDog = async () => {
         
         console.log(data)
     }
-
 }
 //Display Dog information
 const displayDog = (dog) => {      
@@ -52,6 +45,41 @@ const displayDog = (dog) => {
     article.appendChild(header);
     article.appendChild(image);
     dogElement.appendChild(article);  
+    }
+}
+
+
+function addFavorite(){
+    resetFav();
+    favDogs.push(dogList[0]);
+    displayFav(favDogs);
+}
+
+const displayFav = (dog) => {      
+    for (let dogInfo in dog){
+    const article = document.createElement('article');
+    const header = document.createElement('h3');
+    header.innerHTML = dog[dogInfo]["name"];
+    const image = document.createElement('img');
+    image.src = dog[dogInfo]["image_link"];
+    image.alt = dog[dogInfo]["name"];
+    article.appendChild(header);
+    article.appendChild(image);
+    favElement.appendChild(article);  
+    }
+}
+
+function reset(){
+    let parent = document.getElementById("dog");
+    while (parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function resetFav(){
+    let parent = document.getElementById("favoriteDogs");
+    while (parent.firstChild){
+        parent.removeChild(parent.firstChild);
     }
 }
 
